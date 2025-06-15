c4d2e784cd68a7e309f360cdcfa8a45eecdb9997
kakizoeyasuhiro <crt.pt.6@gmail.com>
2025-06-15 12:13:59 +0900

    Works欄の見出しを統一し、アンカーリンクを正しく設定

--- src/components/home/WorksSection.js
+++ src/components/home/WorksSection.js
@@ -8,12 +8,11 @@ export default function WorksSection() {
       <h2 className="section-heading en-text">WORKS</h2>
       
       <div className="works-buttons">
-        <Link href="/works#artist-management" className="work-button ja-text">アーティスト・マネジメント</Link>
-        <Link href="/works#music-production" className="work-button ja-text">音楽制作・作家マネジメント</Link>
-        <Link href="/works#live-event" className="work-button ja-text">ライブ・イベント制作</Link>
-        <Link href="/works#fanclub" className="work-button ja-text">ファンクラブ運営</Link>
-        <Link href="/works#goods" className="work-button ja-text">グッズ制作・デザイン</Link>
-        <Link href="/works#publishing" className="work-button ja-text">音楽出版・出版代行</Link>
+        <Link href="/works#artist-management" className="work-button en-text">ARTIST MANAGEMENT</Link>
+        <Link href="/works#music-contents" className="work-button en-text">MUSIC & CONTENTS</Link>
+        <Link href="/works#live-event" className="work-button en-text">LIVE & EVENT</Link>
+        <Link href="/works#merchandising-fanclub" className="work-button en-text">MERCHANDISING & FANCLUB</Link>
+        <Link href="/works#digital-solution" className="work-button en-text">DIGITAL SOLUTION</Link>
       </div>
       
       <div className="view-detail-link">
