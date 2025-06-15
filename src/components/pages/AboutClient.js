f50795b284518bb1d08cdcb946a53b92f20a117c
kakizoeyasuhiro <crt.pt.6@gmail.com>
2025-06-15 11:40:01 +0900

    Aboutページから所在地の情報を削除

--- src/components/pages/AboutClient.js
+++ src/components/pages/AboutClient.js
@@ -55,25 +55,6 @@ export default function AboutClient() {
             </div>
             
             <div className="info-row">
-              <div className="info-label ja-text">所在地</div>
-              <div className="info-content ja-text">
-                <p>〒213-0002</p>
-                <p>神奈川県川崎市高津区二子2-5-14-102</p>
-                <div className="map-container">
-                  <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3243.747836628691!2d139.61110707483635!3d35.60946818013196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f4f3506d0df1%3A0xee1467d03ab95564!2z44CSMjEzLTAwMDIg56We5aWI5bed55yM5bed5bSO5biC6auY5rSl5Yy65LqM5a2QMi01LTE0!5e0!3m2!1sja!2sjp!4v1717946911345!5m2!1sja!2sjp" 
-                    width="600" 
-                    height="450" 
-                    style={{border:0}} 
-                    allowFullScreen="" 
-                    loading="lazy" 
-                    referrerPolicy="no-referrer-when-downgrade">
-                  </iframe>
-                </div>
-              </div>
-            </div>
-            
-            <div className="info-row">
               <div className="info-label ja-text">代表者</div>
               <div className="info-content ja-text">代表取締役 柿添康大</div>
             </div>
